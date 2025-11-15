120a4475d7d0cb8bf26a1df290ce994ceb18e24f
Dan Brothers <brothersd@hotmail.com>
2025-11-15 13:11:20 -0700

    Guessing Game

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,37 @@
-class GuessingGame():
+import random
+
+class GuessingGame:
+
+    def __init__(self, answer):
+        self.answer = answer
+        self.solved = False
+            
+    def solved(self):
+        return self.solved
+
+    # def guess(self, user_guess):
+    #     self.user_guess = user_guess
+    #     if user_guess == self.answer:
+    #         return "Correct"
+    #     elif user_guess < self.answer:
+    #         return "Low"
+    #     else:
+    #         return "High"
+        
+        
+# ----- DRIVER CODE -----
+game = GuessingGame(random.randint(1,10))
+# game = GuessingGame(10)
+
+user_guess = int(input("Pick a number: "))
+
+while game.solved is False:
+    if user_guess != game.answer:
+        print("Wrong Answer!")
+        game.solved = False
+        user_guess = int(input("Pick a number: "))
+    else:
+        print(f"Correct...Your guess was {user_guess} and the number was {game.answer}")
+        game.solved = True
+
+    
